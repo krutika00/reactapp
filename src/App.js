072b7fa5b4c3bd0abdd6913827ee80bcfd870b11
KRUTIKA PATEL <shreydivy.shrey8194@gmail.com>
2023-08-09 22:51:43 +0530

    md

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          my fist app is succesfuly deploy.
         </a>
       </header>
     </div>
