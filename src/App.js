39d990f15556d975935e941bb15a9946fb718d98
KRUTIKA PATEL <shreydivy.shrey8194@gmail.com>
2023-10-04 14:48:54 +0530

    hey

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          my fist app is succesfuly deploy.
-        </a>
-      </header>
+    <Login  />
     </div>
   );
 }
